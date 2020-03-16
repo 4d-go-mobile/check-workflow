7fe99ec23d00177e62da704933dec9382c42f2da
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2020-03-16 16:46:08 +0100

    Update swift_build.sh

--- form/scripts/swift_build.sh
+++ form/scripts/swift_build.sh
@@ -12,3 +12,6 @@ fi
 sdk=`xcrun -sdk iphonesimulator -show-sdk-path`
 sdkVersion=`echo $sdk | sed -E 's/.*iPhoneSimulator(.*)\.sdk/\1/'`
 swift build  -Xswiftc "-sdk" -Xswiftc "$sdk" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios$sdkVersion-simulator"
+
+# check external SDK version
+curl -fsSL $checkUrl/form/scripts/check_sdk_version.sh | bash -s 5.1.3
